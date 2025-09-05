0606f36175331e3f82c039947ab2279df4b423cb
mr-hoffman <hoffmant@hilbertk12.org>
2025-09-05 11:05:43 -0500

    Create Lab_1_4_2.java

---
+++ Lab_1_4_2.java
@@ -0,0 +1,9 @@
+public class Lab_1_4_2 {
+  public static void main(String[] args) {
+    // An object’s momentum is its mass multiplied by its velocity. 
+    // Write a program that prompts the user for an object’s mass in kilograms 
+    // and velocity (in meters per second) as inputs and prints its momentum.  
+
+    
+  }
+}
