2ad67ccd06b215199d56bb25cc27e5a1abf7490d
mr-hoffman <hoffmant@hilbertk12.org>
2025-09-05 11:12:42 -0500

    Create Lab_1_4_3.java

---
+++ Lab_1_4_3.java
@@ -0,0 +1,10 @@
+public class Lab_1_4_3 {
+  public static void main(String[] args) {
+    // The kinetic energy of a moving object is given by the formula KE = ½ mv2, 
+    // where m is the object’s mass and v is its velocity. Write a program 
+    // that prompts for mass and velocity and prints kinetic energy. 
+    // You do not want a floating point value as your answer. 
+
+  }
+}
+
