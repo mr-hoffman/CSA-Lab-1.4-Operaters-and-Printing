5b7626e5d4bed21d8992bd6d3157d79fc78cfa5a
mr-hoffman <hoffmant@hilbertk12.org>
2025-09-05 11:04:47 -0500

    Create Lab_1_4_1.java

---
+++ Lab_1_4_1.java
@@ -0,0 +1,7 @@
+public class Lab_1_4_1 {
+  public static void main(String[] args) {
+    // Complete a method to convert a time (in minutes) to hours and the remaining minutes. Prompt the user for the number of minutes. 
+
+    
+  }
+}
