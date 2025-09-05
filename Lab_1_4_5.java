738a6c55acd984c47caf349a7296826cd4739179
mr-hoffman <hoffmant@hilbertk12.org>
2025-09-05 11:15:08 -0500

    Create Lab_1_4_5.java

---
+++ Lab_1_4_5.java
@@ -0,0 +1,10 @@
+public class Lab_1_4_5 {
+  public static void main(String[] args) {
+    // Write a program that prompts the user for the number of each 
+    // type of coin and prints the total value. The program should ask 
+    // for four inputs: quarters, dimes, nickels, and pennies (in that order). 
+
+    
+  }
+}
+
