8043b773a944bc8a1d453f1e2ef65953b250e836
mr-hoffman <hoffmant@hilbertk12.org>
2025-09-05 11:14:00 -0500

    Create Lab_1_4_4.java

---
+++ Lab_1_4_4.java
@@ -0,0 +1,12 @@
+public class Lab_1_4_4 {
+  public static void main(String[] args) {
+    // An employee’s total work pay equals the hourly wage multiplied by 
+    // the total number of regular hours plus any overtime pay. 
+    // Overtime pay equals the total overtime hours multiplied by 1.5 times the hourly wage. 
+    // Write a method that takes as parameters the hourly wage, 
+    // total regular hours, and total overtime hours and displays an employee’s total weekly pay. 
+
+    
+  }
+}
+
