28aa090b31c3ea6390c0f83101a7b4866a5a9525
mr-hoffman <hoffmant@hilbertk12.org>
2025-09-05 13:08:11 -0500

    Create Lab_1_4_6.java

---
+++ Lab_1_4_6.java
@@ -0,0 +1,16 @@
+public class Lab_1_4_5 {
+  public static void main(String[] args) {
+    // The shipping clerk at the Rinky Dooflingy Company is faced with 
+    // the following problem:  Dooflingies are very delicate and must be 
+    // shipped in special containers.  These containers are available in 
+    // four sizes:  huge, large, medium, and small, which can hold 
+    // 50, 20, 5, and 1 dooflingy, respectively.  
+    // Your program should allow a user to input any total number of dooflingies 
+    // to be shipped and then display the number of huge, large, medium, and small 
+    // containers needed to send the shipment in the minimum number of containers, 
+    // and with the minimum amount of wasted space. 
+
+
+    
+  }
+}
